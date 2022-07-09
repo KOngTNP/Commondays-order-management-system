cddf0d3f738a9c424c60eb954be917a49503895b
KOngTNP <tanapollll23@gmail.com>
2022-07-09 16:40:28 +0700

    make it very better

--- mysite/views.py
+++ mysite/views.py
@@ -518,3 +518,15 @@ def create_customer(request):
     }
     data['html_form'] = render_to_string('customer/create_customer.html',context, request=request)
     return JsonResponse(data)
+
+def custom_page_not_found_view(request, exception):
+    return render(request, "errors/404.html", {})
+
+def custom_error_view(request, exception=None):
+    return render(request, "errors/500.html", {})
+
+def custom_permission_denied_view(request, exception=None):
+    return render(request, "errors/403.html", {})
+
+def custom_bad_request_view(request, exception=None):
+    return render(request, "errors/400.html", {})
