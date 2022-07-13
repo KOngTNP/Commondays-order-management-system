17b400ddd211efa5af5fed2f481439fc4e51d20b
KOngTNP <tanapollll23@gmail.com>
2022-07-13 16:56:08 +0700

    i think it work

--- Commondaysshop/settings.py
+++ Commondaysshop/settings.py
@@ -156,11 +156,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+AUTH_USER_MODEL = 'login.User'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-AUTH_USER_MODEL = 'login.User'
+
 LOGIN_URL = '/login/'
 
 
