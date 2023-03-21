6e56579b355a1afb82c6918ed90bc2d103760985
KOngTNP <tanapollll23@gmail.com>
2023-03-21 11:25:47 +0700

    config deploy on vercel

--- Commondaysshop/settings.py
+++ Commondaysshop/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'Commondaysshop.wsgi.application'
 #     )
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
