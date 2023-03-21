591bff8e8b7827c1017a959b727e136cffbf9074
KOngTNP <tanapollll23@gmail.com>
2023-03-21 10:18:44 +0700

    config deploy on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo " BUILD START"
 python3.9 -m pip install -U pip
-pip3 install pysqlite3
+# pip3 install pysqlite3
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END"
